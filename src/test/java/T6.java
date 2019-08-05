c11c68b421d7ccb0e149ef73e9fda22725665616
bczhc <bczhc0@126.com>
2019-08-05 20:37:42 +0800

    2 deploy maven repository

--- src/test/java/T6.java
+++ src/test/java/T6.java
@@ -1,5 +1,24 @@
-public class T6 {
+class T9 {
+    int a = 0;
+}
+
+class T6 extends T9 {
+    void f() {
+        this.a = 1;
+    }
+}
+
+class T8 extends T9 {
+    void f() {
+        System.out.println("this.a = " + this.a);
+        this.a = 2;
+        System.out.println("this.a = " + this.a);
+    }
+}
+
+class T7 {
     public static void main(String[] args) {
-        System.out.println("Runtime.getRuntime().availableProcessors() = " + Runtime.getRuntime().availableProcessors());
+        new T6().f();
+        new T8().f();
     }
 }

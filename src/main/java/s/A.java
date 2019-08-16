ed5bef287d2bd590874cd9adde3f2212aa4a534d
bczhc <bczhc0@126.com>
2019-08-17 01:30:16 +0800

    Fourier Series。。and function drawing mvn: 1.3

--- src/main/java/s/A.java
+++ src/main/java/s/A.java
@@ -1,32 +1,7 @@
-package s;/*
 package s;
 
 public class A {
     public static void main(String[] args) {
-        A a = new A();
-        Runnable runnable = a.getRunnable(1);
-        System.out.println("runnable.equals(new Runnable() {\n            @Override\n            public void run() {\n                System.out.println(1);\n            }\n        }) = " + runnable.equals(new Runnable() {
-            @Override
-            public void run() {
-                if (1 == 1) {
-                    System.out.println(1);
-                } else {
-                    System.out.println(".");
-                }
-            }
-        }));
+        System.out.println(new Integer(1).toString());
     }
-
-    private Runnable getRunnable(int o) {
-        return new Runnable() {
-            @Override
-            public void run() {
-                if (o == 1) {
-                    System.out.println(1);
-                } else {
-                    System.out.println(".");
-                }
-            }
-        };
-    }
-}*/
+}
--- src/test/java/T6.java
+++ src/test/java/T6.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Base64;
+
 class T9 {
     int a = 0;
 }
@@ -21,4 +28,100 @@ class T7 {
         new T6().f();
         new T8().f();
     }
+}
+
+class T10 {
+    public static void main(String[] args) throws IOException {
+        File f = new File("/home/zhc/z/\\r.txt");
+        /*OutputStream os = new FileOutputStream(f, false);
+        os.write(new byte[]{97, 0x0D, 97});
+        os.close();*/
+        InputStream is = new FileInputStream(f);
+        byte[] bytes = new byte[10];
+        System.out.println("is.read(bytes) = " + is.read(bytes));
+        System.out.println(Arrays.toString(bytes));
+        is.close();
+    }
+}
+
+class T11 {
+    public static void main(String[] args) {
+        new T11().b();
+    }
+
+    private void b() {
+        int[] a = new int[]{3, 4, 5};
+        int[] b = a(a);
+        System.out.println("a = " + Arrays.toString(a));
+        System.out.println("b = " + Arrays.toString(b));
+    }
+
+    private int[] a(int[] ints) {
+        ints[0] = 1;
+        return ints;
+    }
+}
+
+class T12 {
+    public static void main(String[] args) {
+        byte[] bytes = {23, 108, 61, 15, 11, 9, 57, 124, 0};
+        System.out.println("Base64.getEncoder().encode(bytes) = " + Arrays.toString(Base64.getEncoder().encode(bytes)));
+    }
+}
+
+abstract class DefiniteIntegralApp {
+
+    private double sum;
+    private double e;
+
+    // 定义被积函数，可以修改
+    abstract double f(double x);
+
+    // 定义第i个区间的中点值，即定义积分变量
+    private double zhongjian(double a, double b, double n, int i) {
+        return a + i * (b - a) / n;
+    }
+
+    // 定义每个小区间的间隔差，即将范围分成n个等区间
+    private double cha(double a, double b, double n) {
+        return (b - a) / n;
+    }
+
+    DefiniteIntegralApp(double a, double b, int n) {
+        e = cha(a, b, ((double) n));
+        // 求和，循环从第一个区间叠加到第10000个
+        for (int j = 1; j <= ((double) n); j++) {
+            double x = zhongjian(a, b, ((double) n), j);
+            sum += f(x);
+
+        }
+    }
+
+    double get() {
+        return this.sum * e;
+    }
+}
+
+class T13 {
+    public static void main(String[] args) {
+        double d = new DefiniteIntegralApp(1, 10, 100000000) {
+            @Override
+            double f(double x) {
+//                if (x == 1) return x;
+//                return 0;
+                return x * x;
+            }
+        }.get();
+        System.out.println("d = " + d);
+    }
+}
+
+class T14 {
+    public static void main(String[] args) {
+        new T14().m();
+    }
+
+    private void m() {
+    }
+
 }

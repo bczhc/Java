8231a5794b68c2d337b24d911f0dc46c5061ad3f
bczhc <bczhc0@126.com>
2020-01-12 06:39:54 +0800

    MultipartUploader
    Former-commit-id: 8ac9e1d4b362c7e7c26e268cc4efde3152510d6b

--- src/main/java/pers/zhc/u/common/MultipartUploader.java
+++ src/main/java/pers/zhc/u/common/MultipartUploader.java
@@ -1,6 +1,5 @@
 package pers.zhc.u.common;
 
-import org.jetbrains.annotations.NotNull;
 import pers.zhc.u.FileU;
 
 import java.io.*;
@@ -10,12 +9,9 @@ import java.net.URL;
 public class MultipartUploader {
     /**
      * @param urlStr   url
-     * @param headByte head
+     * @param headByte head information bytes
      * @param is       inputStream
-     * @return response data
-     * 如果head != null，将会上传head+null字节+is数据
-     * 否则上传is数据
-     * 其中{0}作为头和数据的分隔符，便于后台处理
+     * @return 返回response数据
      */
     public static String formUpload(String urlStr, @Documents.Nullable byte[] headByte, InputStream is) throws IOException {
         String res;
@@ -24,12 +20,11 @@ public class MultipartUploader {
         try {
             URL url = new URL(urlStr);
             conn = (HttpURLConnection) url.openConnection();
-            OutputStream out = getOutputStream(conn);
-            headBytePut(headByte, out);
+            OutputStream out = setConnection(conn, headByte);
             // file
             FileU.StreamWrite(is, out);
             is.close();
-            res = left(conn, out);
+            res = getString(conn, out);
         } catch (IOException e) {
             if (conn != null) {
                 conn.disconnect();
@@ -39,15 +34,27 @@ public class MultipartUploader {
         return res;
     }
 
-    private static void headBytePut(@Documents.Nullable byte[] headByte, OutputStream out) throws IOException {
-        if (headByte != null) {
-            out.write(new byte[]{0});
-            out.write(headByte);
+    public static String formUpload(String urlStr, @Documents.Nullable byte[] headByte, byte[] data) throws IOException {
+        String res;
+        HttpURLConnection conn = null;
+        // boundary就是request头和上传文件内容的分隔符
+        try {
+            URL url = new URL(urlStr);
+            conn = (HttpURLConnection) url.openConnection();
+            OutputStream out = setConnection(conn, headByte);
+            out.write(data);
             out.flush();
+            res = getString(conn, out);
+        } catch (IOException e) {
+            if (conn != null) {
+                conn.disconnect();
+            }
+            throw e;
         }
+        return res;
     }
 
-    private static String left(HttpURLConnection conn, OutputStream out) throws IOException {
+    private static String getString(HttpURLConnection conn, OutputStream out) throws IOException {
         String res;
         out.close();
         // 读取返回数据
@@ -62,29 +69,7 @@ public class MultipartUploader {
         return res;
     }
 
-    public static String formUpload(String urlStr, @Documents.Nullable byte[] headByte, @Documents.NotNull byte[] data) throws IOException {
-        String res;
-        HttpURLConnection conn = null;
-        // boundary就是request头和上传文件内容的分隔符
-        try {
-            URL url = new URL(urlStr);
-            conn = (HttpURLConnection) url.openConnection();
-            OutputStream out = getOutputStream(conn);
-            headBytePut(headByte, out);
-            // file
-            out.write(data);
-            res = left(conn, out);
-        } catch (IOException e) {
-            if (conn != null) {
-                conn.disconnect();
-            }
-            throw e;
-        }
-        return res;
-    }
-
-    @NotNull
-    private static OutputStream getOutputStream(HttpURLConnection conn) throws IOException {
+    private static OutputStream setConnection(HttpURLConnection conn, byte[] headByte) throws IOException {
         conn.setConnectTimeout(5000);
         conn.setReadTimeout(30000);
         conn.setDoOutput(true);
@@ -93,6 +78,12 @@ public class MultipartUploader {
         conn.setRequestMethod("POST");
         conn.setRequestProperty("Connection", "Keep-Alive");
         // conn.setRequestProperty("User-Agent","Mozilla/5.0 (Windows; U; Windows NT 6.1; zh-CN; rv:1.9.2.6)");
-        return new DataOutputStream(conn.getOutputStream());
+        OutputStream out = new DataOutputStream(conn.getOutputStream());
+        if (headByte != null) {
+            out.write(headByte);
+            out.write(new byte[]{0});
+            out.flush();
+        }
+        return out;
     }
 }

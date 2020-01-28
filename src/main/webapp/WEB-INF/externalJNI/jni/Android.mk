357744d3a71b2a183ed9b02b97571d8d13dd9a65
bczhc <bczhc0@126.com>
2020-01-28 13:08:18 +0800

    未完成的"伪ssh"..
    Former-commit-id: b78b0e4dc53d2350346420017821de29e4fe15fd

--- src/main/webapp/WEB-INF/externalJNI/jni/Android.mk
+++ src/main/webapp/WEB-INF/externalJNI/jni/Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := ex
-LOCAL_SRC_FILES := ex.c
+LOCAL_MODULE    := ex1
+LOCAL_SRC_FILES := ex1.cpp ./jni_help.cpp ./zhc.cpp
 include $(BUILD_SHARED_LIBRARY)

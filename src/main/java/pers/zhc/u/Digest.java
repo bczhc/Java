c385901dd38ecfe184c60de8627cce26472c2997
bczhc <bczhc0@126.com>
2020-03-10 20:20:26 +0800

    获取文件MD5没有关闭输入流的问题

--- src/main/java/pers/zhc/u/Digest.java
+++ src/main/java/pers/zhc/u/Digest.java
@@ -2,19 +2,29 @@ package pers.zhc.u;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
 
 public class Digest {
     public static byte[] getFileMd5(File file) {
+        InputStream is = null;
         try {
             MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-            InputStream is = new FileInputStream(file);
+            is = new FileInputStream(file);
             byte[] buffer = new byte[1024];
             while (is.read(buffer) != -1) messageDigest.update(buffer);
             return messageDigest.digest();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return null;
     }

465365d58b9647e0a5bca99fc6f0109376a96e8e
bczhc <bczhc0@126.com>
2020-09-19 13:15:32 +0800

    kukuw typing info

--- src/main/webapp/WEB-INF/externalJNI/jni/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := ex1
-LOCAL_SRC_FILES := ex1.cpp ./jni_help.cpp ./zhc.cpp
-include $(BUILD_SHARED_LIBRARY)
--- src/main/webapp/WEB-INF/externalJNI/jni/ex1.cpp
+++
@@ -1,7 +0,0 @@
-#include "./ex1.h"
-
-
-JNIEXPORT void JNICALL Java_pers_zhc_tools_utils_ExternalJNI_ex1
-        (JNIEnv *env, jobject activity) {
-
-}

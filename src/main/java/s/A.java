2467ae022952a849bad398326d0672bdee51358a
bczhc <bczhc0@126.com>
2020-10-05 00:27:06 +0800

    add missing file; sqlite3 test
    Former-commit-id: 94fab9998cb969bd69d5bf138c1f3d8afae2c247 [formerly 151ac3fc36996c502a802b9a1d7ca347bc7599b7]
    Former-commit-id: fd33ca49cbcc791ed1b179819d4decb580f74fb4

--- src/main/java/s/A.java
+++ src/main/java/s/A.java
@@ -3,8 +3,6 @@ package s;
 
 import pers.zhc.tools.utils.MySQLite3;
 
-import java.util.Arrays;
-
 /**
  * @author bczhc
  */
@@ -12,12 +10,22 @@ public class A {
     static {
         System.loadLibrary("bczhcMain");
     }
+
     public static void main(String[] args) {
-        MySQLite3 db = MySQLite3.open("/home/zhc/diary.db");
-        db.exec("SELECT * FROM diary", contents -> {
-            System.out.println(Arrays.toString(contents));
-            return 0;
-        });
+        MySQLite3 db = MySQLite3.open("/tmp/a.db");
+        db.exec("DROP TABLE IF EXISTS a");
+        db.exec("CREATE TABLE IF NOT EXISTS a(a)");
+        db.exec("PRAGMA synchronous = OFF");
+        db.exec("BEGIN TRANSACTION");
+        long start = System.currentTimeMillis();
+        int c = 0;
+        while (System.currentTimeMillis() - start <= 1000) {
+            db.exec("INSERT INTO a VALUES(null)");
+            StringBuilder sb;
+            ++c;
+        }
+        db.exec("COMMIT");
+        System.out.println("c = " + c);
         db.close();
     }
 }

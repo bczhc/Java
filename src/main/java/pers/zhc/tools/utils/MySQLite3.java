43737e462c59764ed8a95fb28089119c3527bb86
bczhc <bczhc0@126.com>
2020-10-04 14:46:26 +0800

    jni sqlite3 test
    Former-commit-id: afa09ff127e7f8e1cd5a9d08d08e02fc72dec1db [formerly 978a69843e72779770fde9400d6773cab32fd35a]
    Former-commit-id: 56570da61c358dcd4f8a745efad2189f9787e5a7

---
+++ src/main/java/pers/zhc/tools/utils/MySQLite3.java
@@ -0,0 +1,52 @@
+package pers.zhc.tools.utils;
+
+import pers.zhc.tools.jni.JNI;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author bczhc
+ */
+public class MySQLite3 {
+    private long id;
+    private boolean isClosed = false;
+
+    private MySQLite3() {
+    }
+
+    public static MySQLite3 open(String path) {
+        final MySQLite3 db = new MySQLite3();
+        db.id = JNI.Sqlite3.open(path);
+        return db;
+    }
+
+    public void close() {
+        JNI.Sqlite3.close(this.id);
+        isClosed = true;
+    }
+
+    public void exec(String cmd, JNI.Sqlite3.SqliteExecCallback callback) {
+        JNI.Sqlite3.exec(this.id, cmd, callback);
+    }
+
+    public void exec(String cmd) {
+        exec(cmd, null);
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+    public boolean hasTable(String tableName) {
+        AtomicBoolean r = new AtomicBoolean(false);
+        try {
+            exec("SELECT name FROM sqlite_master WHERE type='table' AND name='" + tableName + "';", contents -> {
+                r.set(true);
+                return 1;
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return r.get();
+    }
+}
--- src/test/java/demo/Demo5.java
+++ src/test/java/demo/Demo5.java
@@ -1,30 +1,18 @@
 package demo;
 
-import org.junit.Test;
-import pers.zhc.u.common.ReadIS;
-import pers.zhc.u.util.Connection;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Demo5 {
-    @Test
-    public void test() {
+    public static void main(String[] args) {
+        Connection c = null;
         try {
-            URL url = new URL("http://idea.medeming.com/jihuoma/images/jihuoma.txt");
-            URLConnection connection = Connection.get(url, null, null);
-            InputStream is = connection.getInputStream();
-            StringBuilder sb = new StringBuilder();
-            new ReadIS(is, StandardCharsets.UTF_8).read(sb::append);
-            is.close();
-            String s = sb.toString();
-            System.out.println("s = " + s);
-        } catch (IOException e) {
-            e.printStackTrace();
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:~/diary.db");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
         }
-
+        System.out.println("Opened database successfully");
     }
 }

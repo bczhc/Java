00425c1b08d605e65b3374b61b0d31e444e006f1
bczhc <bczhc0@126.com>
2020-11-14 14:57:44 +0800

    online class questions to sqlite3 test
    Former-commit-id: 46ec942265a5dff1499807196d6217e965fea7e0 [formerly 171ebeec083edafb3cb0b51b527414e94cd1c58c]
    Former-commit-id: 7e727f15b9c5f022e04454f19e8663fb0f5fd0a2

--- src/main/java/pers/zhc/u/groovy/Demo0.groovy
+++ src/main/java/pers/zhc/u/groovy/Demo0.groovy
@@ -1,49 +1,38 @@
 package pers.zhc.u.groovy
 
-import org.jdom2.Element
-import org.jdom2.input.SAXBuilder
+import org.json.JSONArray
+import org.json.JSONObject
 import pers.zhc.tools.utils.MySQLite3
+import pers.zhc.u.common.ReadIS
 
-import java.util.regex.Pattern
+import java.nio.charset.StandardCharsets
 
 class Demo0 {
-    static getChildren(Element element, def name) {
-        def children = element.children
-        def r = new LinkedList<Element>()
-        List<Integer> indexes = new LinkedList<>()
-        for (i in 0..<children.size()) {
-            if (children.get(i).name == name) r.add(children.get(i))
-        }
-        return r
-    }
-
     static void main(String[] args) {
-        def db = MySQLite3.open("./students_list")
-        db.exec("DROP TABLE IF EXISTS student")
-        db.exec("CREATE TABLE IF NOT EXISTS student (\n    class_code INTEGER,\n    name TEXT NOT NULL,\n    examinee_number INTEGER PRIMARY KEY\n)")
-        db.exec("BEGIN TRANSACTION")
-        def file = new File("/home/zhc/张家港市乐余高级中学高二学生名单.xml")
-        def builder = new SAXBuilder()
-        def root = builder.build(file).getRootElement()
-        def children = getChildren(root, "Worksheet")
-        children.each { def child ->
-            def tables = getChildren(child, "Table")
-            tables.forEach({ table ->
-                def rows = getChildren(table, "Row")
-                for (i in 1..<rows.size()) {
-                    def row = rows.get(i)
-                    def cells = getChildren(row, "Cell")
-
-                    def matcher = Pattern.compile("(?<=高二年级).+(?=班)").matcher(getChildren(cells[2], "Data")[0].getText())
-                    matcher.find()
-                    int classCode = 200 + (matcher.group(0) as int)
-
-                    def name = getChildren(cells[3], "Data")[0].getText()
-                    def examineeNumber = getChildren(cells[5], "Data")[0].getText() as int
-                    db.exec(String.format("INSERT INTO student VALUES(%d,'%s',%d)", classCode, name, examineeNumber))
-                }
+        def db = MySQLite3.open("./online_class_questions")
+        db.exec("BEGIN TRANSACTION ")
+        db.exec("DROP TABLE IF EXISTS question")
+        db.exec("CREATE TABLE IF NOT EXISTS question (\n    subject_name TEXT NOT NULL,\n    question_owner_name TEXT NOT NULL,\n    question_owner_id INTEGER,\n    question_message TEXT NOT NULL,\n    second_timestamp INTEGER\n)")
+        def file = new File("/home/zhc/questions.json")
+        def is = new FileInputStream(file)
+        def read = ReadIS.readToString(is, StandardCharsets.UTF_8)
+        is.close()
+        def jsonArray = new JSONArray(read)
+        jsonArray.forEach({ JSONObject jsonObject ->
+            def subjectName = jsonObject.getString("subjectName")
+            def courses = jsonObject.getJSONArray("courses")
+            courses.forEach({ JSONObject course ->
+                def questions = course.getJSONObject("questions").getJSONArray("questions")
+                questions.forEach({ JSONObject question ->
+                    def secondTimestamp = question.getInt("secondTimestamp")
+                    def questionText = question.getString("question")
+                    def questionOwnerId = question.getInt("questionOwnerId")
+                    def questionOwner = question.getString("questionOwner")
+                    db.exec(String.format("INSERT INTO question VALUES('%s','%s',%d,'%s',%d)"
+                            , subjectName, questionOwner, questionOwnerId, questionText.replace("'", "''"), secondTimestamp))
+                })
             })
-        }
-        db.exec("COMMIT")
+        })
+        db.exec("COMMIT ")
     }
 }

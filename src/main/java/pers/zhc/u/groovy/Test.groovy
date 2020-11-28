fd2383fb6f314a174a1f741e83c2a796e6a97213
bczhc <bczhc0@126.com>
2020-11-28 16:43:43 +0800

    Make wubi directory database
    Former-commit-id: b6fdb1528394956e2ee3fdad01aa556c4bf94b46 [formerly a40a3c85edc567d09bbeece9ac8a1b82506a9d4b]
    Former-commit-id: e06446ec6af53680f9a961a432c7758aef9c5075

---
+++ src/main/java/pers/zhc/u/groovy/Test.groovy
@@ -0,0 +1,30 @@
+package pers.zhc.u.groovy
+
+import pers.zhc.tools.utils.MySQLite3
+import pers.zhc.u.common.ReadIS
+
+import java.nio.charset.StandardCharsets
+
+class Test {
+    static void main(String[] args) {
+        new File("./wubi_dict.db").delete()
+        def db = MySQLite3.open("./wubi_dict.db")
+        db.exec("BEGIN TRANSACTION")
+        db.exec("CREATE TABLE IF NOT EXISTS wubi_dict (\n    char TEXT NOT NULL,\n    code TEXT NOT NULL,\n    num INTEGER\n)")
+
+        def file = new File("/home/zhc/tmp/rime-wubi/wubi86.dict.txt")
+        def fis = new FileInputStream(file)
+        new ReadIS(fis, StandardCharsets.UTF_8).read({ s ->
+            if (s.matches("^#.*\$")) return
+            def split = s.split('\t')
+            if (split[2].matches("[0-9]+\\.[0-9]+")) split[2] = split[2].substring(0, split[2].indexOf('.'))
+            def num = split[2] as long
+            if (num == 0) return
+            db.exec(String.format("INSERT INTO wubi_dict VALUES('%s','%s',%d)", split[0], split[1], num))
+        })
+
+        db.exec("COMMIT")
+        db.close()
+        fis.close()
+    }
+}
